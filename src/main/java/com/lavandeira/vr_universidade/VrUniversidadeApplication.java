ad4ab6bf1bddbfaacefd331f9fb7cb8bf5c9bb1f
Felipe Lavandeira <felipelavandeira@localhost.localdomain>
2021-01-30 10:26:08 -0300

    Commit inicial com o core da aplicação base

---
+++ src/main/java/com/lavandeira/vr_universidade/VrUniversidadeApplication.java
@@ -0,0 +1,13 @@
+package com.lavandeira.vr_universidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrUniversidadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VrUniversidadeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lavandeira/vr_universidade/VrUniversidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavandeira.vr_universidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrUniversidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
